f9d062c36a28c9e734d3580fe0cb4e2c084c3761
h600868 <600868@stud.hvl.no>
2024-11-07 11:41:01 +0100

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-from sklearn.externals import joblib
-with open('housing_price_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-joblib.dump(model, 'housing_price_model_joblib.pkl')
